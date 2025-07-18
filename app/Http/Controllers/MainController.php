da4df9b370de188a0afbed23bc07a3aa8a8ce020
Caio Felipe Motta Ladeia <caio.felipemotta@hotmail.com>
2025-07-18 15:55:38 -0300

    62. Capturando e Validando IDs Encriptados

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -17,4 +17,34 @@ class MainController extends Controller
   public function newNote() {
     echo '<h1>Create a New Note</h1>';
   }
+
+  public function editNote($id) {
+    try {
+      $id = Crypt::decrypt($id);
+
+      $note = Note::find($id);
+
+      if (!$note) {
+        return redirect()->route('home')->with('error', 'Note not found.');
+      }
+
+      return view('editNote', ['note' => $note]);
+    } catch (\Exception $e) {
+      return redirect()->route('home')->with('error', 'Invalid note ID.');
+    }
+
+  }
+
+  public function deleteNote($id) {
+    $id = Crypt::decrypt($id);
+    $note = Note::find($id);
+
+    if (!$note) {
+      return redirect()->route('home')->with('error', 'Note not found.');
+    }
+
+    $note->delete();
+
+    return redirect()->route('home')->with('message', 'Note deleted successfully.');
+  }
 }
